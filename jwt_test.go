92fa2b40eba6fd4e91b32e4f58937db8c0d38105
ondi <thy321@gmail.com>
2020-02-03 11:13:49 +0300

    minor fix

--- jwt_test.go
+++ jwt_test.go
@@ -26,14 +26,14 @@ func SignVerify(t * testing.T, key string, cert string) {
 	var s Sign_t
 	err := s.LoadKeyPem(key)
 	assert.NilError(t, err, "LOAD KEY")
-	jwt, err := Create(s, 256, payload)
+	token, err := Create(s, 256, payload)
 	assert.NilError(t, err, "JWT CREATE")
-	t.Logf("SignVerify: key=%v, alg=%v, out=%s", key, s.Name(hash_bits), jwt.Bytes())
+	t.Logf("SignVerify: key=%v, alg=%v, out=%s", key, s.Name(hash_bits), token.Bytes())
 	
 	var v Verify_t
 	err = v.LoadCertPem(cert)
 	assert.NilError(t, err, "LOAD CERT")
-	payload, ok, err := Verify(v, jwt.Bytes())
+	payload, ok, err := Verify(v, token.Bytes())
 	assert.NilError(t, err, "VERIFY ERROR")
 	assert.Assert(t, ok, "VERIFY")
 	err = payload.Validate()
